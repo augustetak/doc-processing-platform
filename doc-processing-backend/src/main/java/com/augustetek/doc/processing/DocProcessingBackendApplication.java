85159c33d193d8f506c27c778d6bba9d1e59620c
Augustin Tankam <augustetak@yahoo.fr>
2025-09-09 12:31:10 +0200

    Backend: First commit

---
+++ doc-processing-backend/src/main/java/com/augustetek/doc/processing/DocProcessingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.augustetek.doc.processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocProcessingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocProcessingBackendApplication.class, args);
+	}
+
+}
